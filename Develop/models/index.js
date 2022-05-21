483277307c28477f276a68b6decae5341c54ca52
crumwj22 <crumwj22@hotmail.com>
2022-05-20 23:09:16 -0700

    updated category, index, product models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,8 +1,33 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
+
+Products.belongsToMany(Tags, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+
+  as: "product_tags",
+});
+
+Tags.belongsToMany(Products, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+
+  as: "tags_products",
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
 
 // Products belongsTo Category
 
@@ -11,10 +36,3 @@ const ProductTag = require('./ProductTag');
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
-
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
